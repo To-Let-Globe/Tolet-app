e7753bdfb82f63499ee9193a1f1220ca3dc10da4
raunak234362 <raunakdob@gmail.com>
2024-04-12 20:54:24 +0530

    random

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -4,7 +4,7 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 // import InputGroup from 'react-bootstrap/InputGroup';
 import headerImg from "../assets/img/header-img.png";
-// import backgroundImg from "../assets/image/home.svg"
+import bird1 from "../assets/image/.svg"
 import 'animate.css';
 import { Container } from 'react-bootstrap';
 
@@ -27,7 +27,7 @@ export const Home = () => {
   return (
     <section className={isScrolled ? "home scrolled" : "home"} id="home">
       <Container className="top-home" >
-      {/* <img src={backgroundImg} alt="Background Img" className="img-2" /> */}
+      <img src={bird1} alt="Background Img" className="img-2" />
         <h1 className="tagline">Welcome to To-Let Globe</h1>
         <h6 className="headline">
           {`NO BROKERAGE ON`}
