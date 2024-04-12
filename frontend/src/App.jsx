82efa558e2f9050da31b6fc4f2556aa4f1baa494
baani171102 <baanipreetkaur171102@gmail.com>
2024-04-12 21:23:59 +0530

    Blog page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { NavBar } from './components/navbar';
 import { Home } from './components/home';
 import { Contact } from './components/contactus';
 import { Login } from './components/login';
+import { Blog } from './components/blog';
 import { Footer} from './components/footer'
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -16,7 +17,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/login/*" element={<Login />} />
+          <Route path="/login*" element={<Login />} />
+          <Route path="/blog*" element={<Blog />} />
         </Routes>
         <Footer />
       </BrowserRouter>
---
+++ frontend/src/components/blog.jsx
@@ -0,0 +1,176 @@
+
+export const Blog = () =>{
+return(
+    <Container>
+    <div class="line"></div>
+    <h1 class="insights">
+        Our Insights and Stories
+    </h1>
+    <div class="lorem">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    </div>
+    <div class="content">
+    <div class="trending">
+       <div class="cl">Trending</div>
+       <div class="cla"> Latest</div>
+    </div>
+    <div class="box">
+        <div class="boxes">
+        <div class="box-img" style="background-image: url('HD-wallpaper-notebook-pinterest-working-business-wireless-smartphone-technology.jpg');">
+            </div>
+        <div class="box-content">
+            <h2 class="heading">Collaboration and Partnership Update!</h2>
+            <div class="date">
+                2 Nov, 23 
+                <div class="vertical-line">|</div><div class="featured">Featured Updates</div>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero reprehenderit natus, voluptas at amet minus ab, deserunt molestiae, nemo asperiores ipsa nihil possimus! Nam, omnis voluptates reiciendis distinctio officia fugiat!</p>
+        <p><a class="readmore">Read More</a></p>
+        <div class="box-footer">
+        <div class="short-image"></div>
+            <div class="name">Bhaumik Kore</div>
+        </div>
+            <div class="designation">Full Stack Developer</div>
+        </div>
+        <hr>
+    </div>
+    <div class="boxes">
+        <div class="box-img" style="background-image: url('HD-wallpaper-notebook-pinterest-working-business-wireless-smartphone-technology.jpg');">
+            </div>
+        <div class="box-content">
+            <h2 class="heading">Collaboration and Partnership Update!</h2>
+            <div class="date">
+                2 Nov, 23 
+                <div class="vertical-line">|</div><div class="featured">Featured Updates</div>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero reprehenderit natus, voluptas at amet minus ab, deserunt molestiae, nemo asperiores ipsa nihil possimus! Nam, omnis voluptates reiciendis distinctio officia fugiat!</p>
+        <p><a class="readmore">Read More </a></p>
+        <div class="box-footer">
+        <div class="short-image"></div>
+            <div class="name">Bhaumik Kore</div>
+        </div>
+            <div class="designation">Full Stack Developer</div>
+        </div>
+        <hr>
+    </div>
+    <div class="boxes"> 
+    <div class="box-img" style="background-image: url('HD-wallpaper-notebook-pinterest-working-business-wireless-smartphone-technology.jpg');">
+            </div>
+        <div class="box-content">
+            <h2 class="heading">Collaboration and Partnership Update!</h2>
+            <div class="date">
+                2 Nov, 23 
+                <div class="vertical-line">|</div><div class="featured">Featured Updates</div>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero reprehenderit natus, voluptas at amet minus ab, deserunt molestiae, nemo asperiores ipsa nihil possimus! Nam, omnis voluptates reiciendis distinctio officia fugiat!</p>
+        <p><a class="readmore fa-solid fa-arrow-right">Read More </a><i></i>
+        </p>
+        <div class="box-footer">
+            <div class="short-image"></div>
+                <div class="name">Bhaumik Kore</div>
+            </div>
+                <div class="designation">Full Stack Developer</div>
+            
+        </div>
+        <hr>
+        </div>
+    <div class="boxes"> 
+    <div class="box-img" style="background-image: url('HD-wallpaper-notebook-pinterest-working-business-wireless-smartphone-technology.jpg');">
+            </div>
+        <div class="box-content">
+            <h2 class="heading">Collaboration and Partnership Update!</h2>
+            <div class="date">
+                2 Nov, 23 
+                <div class="vertical-line">|</div><div class="featured">Featured Updates</div>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero reprehenderit natus, voluptas at amet minus ab, deserunt molestiae, nemo asperiores ipsa nihil possimus! Nam, omnis voluptates reiciendis distinctio officia fugiat!</p>
+        <p><a class="readmore fa-solid fa-arrow-right">Read More </a><i></i>
+        </p>
+        <div class="box-footer">
+            <div class="short-image"></div>
+                <div class="name">Bhaumik Kore</div>
+            </div>
+                <div class="designation">Full Stack Developer</div>
+        </div>
+        <hr>
+        </div>
+    <div class="boxes"> 
+    <div class="box-img" style="background-image: url('HD-wallpaper-notebook-pinterest-working-business-wireless-smartphone-technology.jpg');">
+            </div>
+        <div class="box-content">
+            <h2 class="heading">Collaboration and Partnership Update!</h2>
+            <div class="date">
+                2 Nov, 23 
+                <div class="vertical-line">|</div><div class="featured">Featured Updates</div>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero reprehenderit natus, voluptas at amet minus ab, deserunt molestiae, nemo asperiores ipsa nihil possimus! Nam, omnis voluptates reiciendis distinctio officia fugiat!</p>
+        <p><a class="readmore fa-solid fa-arrow-right">Read More </a><i></i>
+        </p>
+        <div class="box-footer">
+            <div class="short-image"></div>
+                <div class="name">Bhaumik Kore</div>
+            </div>
+                <div class="designation">Full Stack Developer</div>
+        </div>
+        <hr>
+        </div>
+    <div class="boxes"> 
+    <div class="box-img" style="background-image: url('HD-wallpaper-notebook-pinterest-working-business-wireless-smartphone-technology.jpg');">
+            </div>
+        <div class="box-content">
+            <h2 class="heading">Collaboration and Partnership Update!</h2>
+            <div class="date">
+                2 Nov, 23 
+                <div class="vertical-line">|</div><div class="featured">Featured Updates</div>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero reprehenderit natus, voluptas at amet minus ab, deserunt molestiae, nemo asperiores ipsa nihil possimus! Nam, omnis voluptates reiciendis distinctio officia fugiat!</p>
+        <p><a class="readmore fa-solid fa-arrow-right">Read More </a><i></i>
+        </p>
+        <div class="box-footer">
+            <div class="short-image"></div>
+                <div class="name">Bhaumik Kore</div>
+            </div>
+                <div class="designation">Full Stack Developer</div>
+        </div>
+        <hr>
+        </div>
+     </div>
+     <div class="page-bar">
+        <div class="previous">Previous</div>
+         <a href="" class="one">1</a>
+        <a href="" class="two" >2</a>
+        <a href="" class="three"> 3</a>
+        <a href=""class="next" >Next</a>
+          </div>
+          <div class="categories">Categories</div>
+          
+          <div class="columimg">
+            <div class="coloumbox">
+                <img src="./1.png" alt="img" width="300px">
+            <a href="">News & Events</a>
+            </div>
+            <div class="coloumbox">
+                <img src="./2.png" alt="img" width="300px">
+            <a href="">Feature Updates</a>
+            </div>
+            <div class="coloumbox">
+                <img src="./3.png" alt="img" width="300px">
+            <a href="">Brand Collaboration</a>
+            </div>
+            <div class="coloumbox">
+                <img src="./4.png" alt="img" width="300px">
+            <a href="">Life at To-Let</a>
+            </div>
+            <div class="coloumbox">
+                <img src="./5.png" alt="img" width="300px">
+            <a href="">Internship Campaigns</a>
+            </div>
+            <div class="coloumbox">
+                <img src="./6.png" alt="img" width="300px">
+            <a href="">Giveway & Offers</a>
+            </div>
+        </div>
+        </Container>
+   
+)
+}
