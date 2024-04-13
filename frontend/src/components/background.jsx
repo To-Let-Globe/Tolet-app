af1bfda1058f7dffc6a4450517e561103fb6da0e
raunak234362 <raunakdob@gmail.com>
2024-04-13 13:03:45 +0530

    Updating images

---
+++ frontend/src/components/background.jsx
@@ -0,0 +1,37 @@
+// ------------Home Img-------------
+import home1 from "../assets/image/home/home1.svg"
+import home2 from "../assets/image/home/home2.svg"
+import home3 from "../assets/image/home/home3.svg"
+import home4 from "../assets/image/home/home4.svg"
+import home5 from "../assets/image/home/home5.svg"
+import home6 from "../assets/image/home/home6.svg"
+import home7 from "../assets/image/home/home7.svg"
+import home8 from "../assets/image/home/home8.svg"
+import home9 from "../assets/image/home/home9.svg"
+import home10 from "../assets/image/home/home10.svg"
+import home11 from "../assets/image/home/home11.svg"
+import home12 from "../assets/image/home/home12.svg"
+import home13 from "../assets/image/home/home13.svg"
+
+// -----------Birds Img------------
+
+
+export const Images =() =>{
+    return(
+        <div className="image-container-1">
+        <img src={home1} alt="Home Img" className="img-1" />
+        <img src={home2} alt="Home Img" className="img-1" />
+        <img src={home3} alt="Home Img" className="img-1" />
+        <img src={home4} alt="Home Img" className="img-1" />
+        <img src={home5} alt="Home Img" className="img-1" />
+        <img src={home6} alt="Home Img" className="img-1" />
+        <img src={home7} alt="Home Img" className="img-1" />
+        <img src={home8} alt="Home Img" className="img-1" />
+        <img src={home9} alt="Home Img" className="img-1" />
+        <img src={home10} alt="Home Img" className="img-1" />
+        <img src={home11} alt="Home Img" className="img-1" />
+        <img src={home12} alt="Home Img" className="img-1" />
+        <img src={home13} alt="Home Img" className="img-1" />
+        </div>
+    )
+}
