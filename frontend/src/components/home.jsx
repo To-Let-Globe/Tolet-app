94c1ad1505d88889e1a432b387cef5676a18a077
Priti-12171 <priti.yadav20920@gmail.com>
2024-04-13 22:36:48 +0530

    hi

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -39,7 +39,7 @@ export const Home = () => {
       <div className="scrollable-content">
 
         <Container className='bottom-home'>
-          <h1 className="tagline2">Welcome to To-Let </h1>
+          <h1 className="tagline2">Welcome to To-Let Priti </h1>
           <h6 className="headline2">
             {`NO BROKERAGE ON`}
             <span className="wrap"> PGS | FLATS | HOUSES | OFFICES </span>
