4259eec236d7e5402b0793539b5942a7008f3f2b
raunak234362 <raunakdob@gmail.com>
2024-04-15 11:34:16 +0530

    Setting up the images

--- frontend/src/components/background.jsx
+++ frontend/src/components/background.jsx
@@ -1,22 +1,27 @@
+import "../style/background.css"
 // ------------Home Img-------------
-import home1 from "../assets/image/home/home1.svg"
-import home2 from "../assets/image/home/home2.svg"
-import home3 from "../assets/image/home/home3.svg"
-import home4 from "../assets/image/home/home4.svg"
-import home5 from "../assets/image/home/home5.svg"
-import home6 from "../assets/image/home/home6.svg"
-import home7 from "../assets/image/home/home7.svg"
-import home8 from "../assets/image/home/home8.svg"
-import home9 from "../assets/image/home/home9.svg"
-import home10 from "../assets/image/home/home10.svg"
-import home11 from "../assets/image/home/home11.svg"
-import home12 from "../assets/image/home/home12.svg"
-import home13 from "../assets/image/home/home13.svg"
+// import home1 from "../assets/image/home/home1.svg"
+// import home2 from "../assets/image/home/home2.svg"
+// import home3 from "../assets/image/home/home3.svg"
+// import home4 from "../assets/image/home/home4.svg"
+// import home5 from "../assets/image/home/home5.svg"
+// import home6 from "../assets/image/home/home6.svg"
+// import home7 from "../assets/image/home/home7.svg"
+// import home8 from "../assets/image/home/home8.svg"
+// import home9 from "../assets/image/home/home9.svg"
+// import home10 from "../assets/image/home/home10.svg"
+// import home11 from "../assets/image/home/home11.svg"
+// import home12 from "../assets/image/home/home12.svg"
+// import home13 from "../assets/image/home/home13.svg"
 import { Container } from "react-bootstrap"
 
 // -----------Birds Img------------
-// import bird1 from "../assets/image/birds/bird1.svg";
-// import bird2 from "../assets/image/birds/bird2.svg";
+import bird1 from "../assets/image/birds/bird1.svg";
+import bird2 from "../assets/image/birds/bird2.svg";
+
+// ---------- Background Img----------
+import bg1 from "../assets/image/background/bg1.svg";
+import bg2 from "../assets/image/background/bg2.svg";
 
 export const Images =() =>{
     return(
@@ -24,30 +29,39 @@ export const Images =() =>{
 
             {/* -----------------Top Component----------------- */}
             <div className="image-container-1">
-                {/* <div className="bird-img">
-                    <img src={bird1} alt="Home Img" className="img-1" />
-                    <img src={bird2} alt="Home Img" className="img-1" />
-                </div> */}
-                {/* <div className="top-img">
-                    <img src={home1} alt="Home Img" className="thome-1" />
-                    <img src={home2} alt="Home Img" className="thome-2" />
-                    <img src={home3} alt="Home Img" className="thome-3" />
-                    <img src={home4} alt="Home Img" className="thome-4" />
-                    <img src={home5} alt="Home Img" className="thome-5" />
-                    <img src={home6} alt="Home Img" className="thome-6" />
-                    <img src={home7} alt="Home Img" className="thome-7" />
-                    <img src={home8} alt="Home Img" className="thome-8" />
-                    <img src={home9} alt="Home Img" className="thome-9" />
-                    <img src={home10} alt="Home Img" className="thome-10" />
-                    <img src={home11} alt="Home Img" className="thome-11" />
-                    <img src={home12} alt="Home Img" className="thome-12" />
-                    <img src={home13} alt="Home Img" className="thome-13" />
-                </div> */}
+                
+                <div className="top-img">
+                    <div className="bg-img back">
+                        <img src={bg2} alt=""  className="bg-2"/>
+                        <div className="front">
+                            <img src={bg1} alt=""  className="bg-1"/>
+                        </div>
+                    </div>
+                    <div className="bird-img">
+                        <img src={bird1} alt="Home Img" className="bird-1" />
+                        <img src={bird2} alt="Home Img" className="bird-2" />
+                    </div>
+                    {/* <div className="home-img">
+                        <img src={home1} alt="Home Img" className="thome-1" />
+                        <img src={home2} alt="Home Img" className="thome-2" />
+                        <img src={home3} alt="Home Img" className="thome-3" />
+                        <img src={home4} alt="Home Img" className="thome-4" />
+                        <img src={home5} alt="Home Img" className="thome-5" />
+                        <img src={home6} alt="Home Img" className="thome-6" />
+                        <img src={home7} alt="Home Img" className="thome-7" />
+                        <img src={home8} alt="Home Img" className="thome-8" />
+                        <img src={home9} alt="Home Img" className="thome-9" />
+                        <img src={home10} alt="Home Img" className="thome-10" />
+                        <img src={home11} alt="Home Img" className="thome-11" />
+                        <img src={home12} alt="Home Img" className="thome-12" />
+                        <img src={home13} alt="Home Img" className="thome-13" />
+                    </div> */}
+                </div>
             </div>
 
 
             {/* -----------Bottom components--------------- */}
-            <div className="image-container-2">
+            {/* <div className="image-container-2">
                 <div className="top-img">
                     <img src={home1} alt="Home Img" className="thome-1" />
                     <img src={home2} alt="Home Img" className="thome-2" />
@@ -63,7 +77,7 @@ export const Images =() =>{
                     <img src={home12} alt="Home Img" className="thome-12" />
                     <img src={home13} alt="Home Img" className="thome-13" />
                 </div>
-            </div>
+            </div> */}
         </Container>
     )
 }
--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -28,17 +28,22 @@ export const Home = () => {
     <section className={isScrolled ? "home scrolled" : "home"} id="home">
       <Container className="top-home" >
       
-        <h1 className="tagline">Welcome to To-Let Globe Priti </h1>
+        <h1 className="tagline">Welcome to To-Let Globe </h1>
         <h6 className="headline">
           {`NO BROKERAGE ON`}
           <span className="wrap"> PGS | FLATS | HOUSES | OFFICES </span>
         </h6>        
-       <Images/>
+        <div className="image-container-1">
+            <Images/>
+        </div>  
       </Container>
 
       <div className="scrollable-content">
         <Container className='bottom-home'>
-          <h1 className="tagline2">Welcome to To-Let Priti </h1>
+        {/* <div className="image-container-2">
+            <Images/>
+            </div>     */}
+          <h1 className="tagline2">Welcome to To-Let Globe </h1>
           <h6 className="headline2">
             {`NO BROKERAGE ON`}
             <span className="wrap"> PGS | FLATS | HOUSES | OFFICES </span>
@@ -50,9 +55,7 @@ export const Home = () => {
               />
               <Button onClick={() => console.log("connect")}>Search</Button>
           </div>
-            <div className="image-container-2">
-            <Images/>
-            </div>    
+            
         </Container>
       </div>
     </section>
