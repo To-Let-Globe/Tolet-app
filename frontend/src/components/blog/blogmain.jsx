7db94d6db04695acdbaf857f526899f7475d6de0
raunak234362 <raunakdob@gmail.com>
2024-06-15 15:12:00 +0530

    chore: Update dependencies and remove unused code

--- frontend/src/components/blog/blogmain.jsx
+++ frontend/src/components/blog/blogmain.jsx
@@ -1,29 +1,24 @@
-import { BiRightArrow } from "react-icons/bi"
-import {BlogPage} from "../../SampleData"
+import { BiRightArrow } from "react-icons/bi";
+import { BlogPage } from "../../SampleData";
 
 export const BlogMain = () => {
   return (
-    
-    <div className="bg-black">
-         <h1 className="text-4xl text-center font-semibold mt-4">To-Let Tales</h1>
-         <h1 className="text-center mt-2 text-[#6CC1B6]">Dive into a Sea of Endless Stories and Insights</h1>
-         <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 justify-around gap-5 px-8 mb-8 mt-10 overflow-x-auto w-full">
-          {
-            BlogPage.map((data,index)=>(
-                <div key={index} className=" text-white rounded-xl m-5 flex flex-col justify-center">
-                    <img src={data?.image1} alt="" className="rounded-xl" />
-                    <p className="text-gray-400">{data?.date}</p>
-                    <h1 className="text-[#6CC1B6] font-semibold text-xl">{data?.title.split(" ").slice(0,10).join(" ")}{data?.title.split(" ").length>10 && "..."}</h1>
-                    <h1 className="text-[#6CC1B6] font-semibold text-xl"></h1>
-                    <h1 className="text-white">{data?.des1.split(" ").slice(0, 30).join(" ")}{data?.des1.split(" ").length > 30 && "..."}</h1>
-                    <h1 className="flex items-center gap-2 text-[#6CC1B6]">Read More <BiRightArrow /></h1>
-                    <h1 className="text-white">{data?.author}</h1>
-                    <h1 className="text-white">{data?.designation}</h1>
-                </div>
-            ))
-          }
-        </div>
+    <div className="bg-black text-white py-8">
+      <h1 className="text-4xl text-center font-semibold mt-4">To-Let Tales</h1>
+      <h1 className="text-center mt-2 text-[#6CC1B6]">Dive into a Sea of Endless Stories and Insights</h1>
+      <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 px-5 mb-8 mt-10 w-full">
+        {BlogPage.map((data, index) => (
+          <div key={index} className=" rounded-xl p-5 flex flex-col">
+            <img src={data?.image1} alt="" className="rounded-xl mb-4" />
+            <p className="text-gray-400 mb-2">{data?.date}</p>
+            <h1 className="text-[#6CC1B6] font-semibold text-xl mb-2">{data?.title.split(" ").slice(0, 10).join(" ")}{data?.title.split(" ").length > 10 && "..."}</h1>
+            <p className="mb-4">{data?.des1.split(" ").slice(0, 10).join(" ")}{data?.des1.split(" ").length > 10 && "..."}</p>
+            <p className="flex items-center text-sm gap-2 text-[#6CC1B6] cursor-pointer mb-4">Read More <BiRightArrow /></p>
+            <p className="text-gray-400">{data?.author}</p>
+            <p className="text-gray-400">{data?.designation}</p>
+          </div>
+        ))}
+      </div>
     </div>
-       
-  )
-}
+  );
+};
